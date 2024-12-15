2798ea7d8bcb9845aa36fb4ab682399497e2e005
AgentePanela <agentepanela@gmail.com>
2024-12-14 22:29:49 -0300

    https test 1

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const path = require('path');
 const serveIndex = require('serve-index');
+
 const app = express();
-const port = 1414;
+const port = 3000;
 
+// cert
+const privateKey = fs.readFileSync('/etc/letsencrypt/private.key', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/server.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
+// replay dir
 const replayDirectory = '/home/gabystation/watchdog/SS14.Watchdog/bin/instances/gabynatal/data/replays';
 
-
+// replay list
 app.use('/replays', express.static(replayDirectory), serveIndex(replayDirectory, {'icons': true}));
 
-
 app.get('/', (req, res) => {
-    res.send('<h1>Servidor de Replays do SS14</h1>');
+    res.send('<h1>Gabystation replays</h1>');
 });
 
-app.listen(port, () => {
-    console.log(`Servidor de replays rodando em http://localhost:${port}`);
+// Criando o servidor HTTPS
+https.createServer(credentials, app).listen(port, () => {
+    console.log(`Running in: http(s)://localhost:${port}`);
 });
