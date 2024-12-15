e53f26f1592b6c30fea9c10c5f8d1992582c2368
AgentePanela <agentepanela@gmail.com>
2024-12-14 22:59:28 -0300

    fix certificate issues

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const path = require('path');
 const serveIndex = require('serve-index');
 
 const app = express();
-const port = 3000;
+const port = 1414;
 
 // cert
-const privateKey = fs.readFileSync('/etc/letsencrypt/private.key', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/server.crt', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/tucanostation.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/tucanostation.com/fullchain.pem', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
 // replay dir
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
     res.send('<h1>Gabystation replays</h1>');
 });
 
-// Criando o servidor HTTPS
+// server running :)
 https.createServer(credentials, app).listen(port, () => {
     console.log(`Running in: http(s)://localhost:${port}`);
 });
